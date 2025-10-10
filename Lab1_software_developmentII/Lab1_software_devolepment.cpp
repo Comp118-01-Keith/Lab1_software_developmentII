5d1b35387d5971991d2854b30fd6a6f149597e42
keithkimani06 <keithkimani06@gmail.com>
2025-10-10 11:23:47 +0300

    Hello world

---
+++ Lab1_software_developmentII/Lab1_software_devolepment.cpp
@@ -0,0 +1,16 @@
+//lab0-comp118.cpp:This file contains the 'main' function.Program execution begins//
+
+#include<iostream>
+
+using namespace std;
+
+//Starting point
+
+int main() {
+
+	cout << "Hello World";
+
+	
+}
+
+
